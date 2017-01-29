867698c6a6dd6aba52511b6f8278b7d3c8da27c5
dermot <dermot.kilroy@invoko.co.uk>
2017-01-29 21:27:00 +0000

    added some more coins

--- test/snackMachineTests.js
+++ test/snackMachineTests.js
@@ -3,34 +3,41 @@ var should = require('chai').should();
 describe("SnackMachine", () => {
     describe("inserting Money", () => {
         it("should be one pound1 when one pound coin is inserted", function() {
-            SnackMachine.insert(Money.pound());
-            SnackMachine.getTotal().should.equal(1.0);
+            SnackMachine.insert(Money.penny);
+            SnackMachine.insert(Money.fivePence);
+            SnackMachine.insert(Money.tenPence);
+            SnackMachine.insert(Money.pound);
+            SnackMachine.getTotal().should.equal(1.16);
         });
     });
 
     describe("returning Money", () => {
-        it("should be one pound1 when one pound coin is inserted", function() {
+        it("should implemented", function() {
             //Implement assertions
         });
     });
 });
 
 let SnackMachine = (function () {
-    let totalAmount;
+    let totalAmount = 0;
     return {
         getTotal: function(){
-            return totalAmount;
+            return Math.round(totalAmount * 1e2) / 1e2;
         },
         insert : function(amount) {
-            totalAmount = amount;
+            totalAmount += amount;
         }
     };
 }());
 
 let Money = (function (){
     return {
-        pound: function () {
-            return 1.0;
-        }
-    };
+        penny: 0.01,
+        fivePence:0.05,
+        tenPence:0.10,
+        pound:1.0,
+        fivePound:5.0,
+        tenPound:10.0,
+        twentyPound:20.0,
+    }
 }());
